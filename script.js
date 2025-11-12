1bf4833d834ca3a5c23eea2bf4cdf04c88281753
KalpX <kalp207987@student-schoolsofwestfield.org>
2025-11-12 14:02:25 +0000

    ghjgh

--- script.js
+++ script.js
@@ -33,47 +33,49 @@ alert("An " + animalType + " has " + legs + " legs ");
 // Multiply the current value of legs by 4 and assign that to legs
 legs = 32;
 // Assign the name of a type of animal which could have the new value of legs to animalType
-
+animalType = "Centipede";
 // Display an Alert Box with the message: "An [animalType] SOMETIMES has [legs] legs"
-
+alert("An " + animalType + " SOMETIMES has " + legs + " legs ");
 // Assign the value of 3 to legs
-
+legs = "3";
 // Assign the string "amputee" to animalType
-
+animalType = "Amputee";
 // Display an Alert Box with the message: "An [animalType] SOMETIMES has [legs] legs"
-
+alert("An " + animalType + " SOMETIMES has " + legs + " legs ");
 // Assign the string "dog" to animalType
-
+animalType = "dog";
 // Display an Alert Box with the message: "for example, an [animalType]".
-
+alert("for example, " + " an " + animalType);
 /* Fun with expressions
 * Understanding the pecularities of the JavaScript Alert Box
 */
 
 // Declare a variable and name it first
-
+var first;
 // Assign a value to first using a prompt: "Enter first number"
-
+first = prompt("Enter First Number");
 // Declare a variable and name it second
-
+var second;
 // Assign a value to second using a prompt: "Enter second number"
-
+second = prompt("Enter Second Number");
 // Declare a variable and name it bragging
-
+var bragging;
 // Assign exactly this expression to bragging: "I can add " + first + " + " + second"
-
+bragging = "I can add " + first + " + " + second;
 // Display an Alert Box with bragging inside it.
-
+alert(bragging);
 // Assign exactly this expression to bragging: first + " + " + second + " = " + first + second
-
+var answer;
+answer = (first + second);
+bragging = first + " + " + second + " = " + first + second + answer;
 // DO NOT Display an Alert Box with bragging inside it YET.
-
+//prediction = first + " + " + second + " = " + first + second;
 // Declare a variable and name it prediction
-
+var prediction;
 // Assign your prediction about what will be displayed when we display bragging
-
+alert(bragging)
 // Display an Alert Box with prediction inside it.
-
+alert(prediction);
 // Call Mr. M. over to see it. Do not do anythign else!
 
 // Await further instructions.
